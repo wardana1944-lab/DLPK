d065783a220572095e8dd90d52e4d5a464d62e98
wardana1944-lab <wardana1944@gmail.com>
2026-03-29 15:15:02 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,11 @@
 self.addEventListener('install', (event) => {
-    console.log('[SW] Service Worker Terinstall');
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    console.log('[SW] Service Worker Aktif');
     return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-    // Biarkan request jaringan berjalan normal (tanpa offline cache sementara ini)
+    // Biarkan request jaringan berjalan normal
 });
